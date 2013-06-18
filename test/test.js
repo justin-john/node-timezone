8048dbb1a795e8bfbe5a0d1acfdcb621870523af
Justin John <justin.johnzz@gmail.com>
2013-06-18 19:39:06 +0530

    Add npm ignore file

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ var assert = require('assert'),
     timezoner = require('../lib/timezone');
 
 timezoner.getTimeZone(
-	39.6034810, // Latitude coordinate
-	-119.6822510, // Longitude coordinate
-	function (err, data) {
-		assert.ok(data);
-		assert.equal(data.status, 'OK');
-		assert.equal(data.timeZoneId, 'America/Los_Angeles');
-	}
+    39.6034810, 
+    -119.6822510, 
+    function (err, data) {
+        assert.ok(data);
+        assert.equal(data.status, 'OK');
+        assert.equal(data.timeZoneId, 'America/Los_Angeles');
+    }
 );
